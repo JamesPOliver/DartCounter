a3675aeb4900a6f9f82f94efafce687159102d5b
JamesPOliver <48917029+JamesPOliver@users.noreply.github.com>
2019-03-29 20:58:29 +0000

    Update Darts v1.py

--- Darts v1.py
+++ Darts v1.py
@@ -8,6 +8,7 @@ while p1_score != 0 and p2_score != 0:
     print(p1_name)
     for i in range(0, 3):
         temp_score = int(input("Enter score from dart: "))
+        p1_score -= temp_score
     print(p1_score)
     print(p2_name)
     for i in range(0, 3):
